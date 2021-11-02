7c6b1670549a28859c70c996be9ec0677e654067
sheng1124 <rhythm7321@gmail.com>
2021-11-03 00:34:59 +0800

    modify test

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
-from src.db.sql import *
+from src.db_operation import *
 
-connect_to_db(database = 'komame_bot')
+
+lbdi = Line_bot_db_inserter('komame_bot')
+
+
+bot_info = {}
+bot_info['name'] = input("名稱\n")
+bot_info['user_id'] = input("bot user id\n")
+bot_info['channel_access_token'] = input("channel access token\n")
+bot_info['channel_secret'] = input("channel secret\n")
+bot_info['webhook_dns'] = input("webhook 網域名稱\n")
+
+lbdi.insert_line_bot_info(bot_info)
